aaf38f06a382c54eeea359370d2bc5ae8bab2508
codebyvimal <vimworks.ai@gmail.com>
2025-09-19 18:23:28 +0530

    Optimized my code
    I defined new functions to make the code good, maybe.

--- Week - 2/MiniGame.py
+++ Week - 2/MiniGame.py
@@ -1,22 +1,31 @@
 import random
-print("Welcome to Stone Paper Scissors")
+def main():
+     print("Welcome to Stone Paper Scissors")
 
-Computer = ["stone","paper","scissors"]
-Decision = random.choice(Computer)
+     Computer = ["stone","paper","scissors"]
+     Decision = random.choice(Computer)
 
-user = input("Type your choice ").lower()
+     user = input("Type your choice ").lower()
 
-print("Computer chooses " + Decision)
+     print("Computer chooses " + Decision)
 
-if (user == "stone" and Decision == "paper"):
+     if (user == "stone" and Decision == "paper"):
+          lose()
+     elif(user == "scissors" and Decision == "stone"):
+           lose()
+     elif(user == "paper" and Decision == "scissors"):
+          lose()
+     elif user == Decision:
+          draw()
+     elif user != "stone" or user != "paper" or user != "scissors":
+          print("Wait, What?🤔")
+     else:
+          win() 
+
+def lose():
      print("You Lose 😟")
-elif(user == "scissors" and Decision == "stone"):
-     print("You Lose 😟")
-elif(user == "paper" and Decision == "scissors"):
-    print("You Lose 😟")
-elif user == Decision:
-     print("Draw ✋")
-elif user != "stone" or user != "paper" or user != "scissors":
-     print("Wait, What?🤔")
-else:
+def win():
      print("You Win 🏆")
+def draw():
+     print("Draw ✋")
+main()
